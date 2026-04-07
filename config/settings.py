92ae84f0fcbc55dc8946a74188dc90d6c2b7722c
JamieAtGit <jamie3.young@live.uwe.ac.uk>
2026-04-07 01:25:29 +0100

    add JWT authentication to REST API

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,7 @@ AUTH_USER_MODEL = 'marketplace.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('marketplace.urls')),
     path('api/', include('marketplace.api_urls')),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
